c3ecc7130380be6d62afc69aae1d51b88a59f7d9
Daniel Palencia <Lionheart920@hotmail.com>
2019-10-26 00:09:38 -0700

    Add high and low

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -4,13 +4,14 @@ const forecast = (latitude, longitude , callback) => {
   const url = 'https://api.darksky.net/forecast/9f4c425790c22593f23e45e7120458ef/' + encodeURIComponent(latitude) + ','+ encodeURIComponent(longitude)
 
   request({url, json: true}, (error, { body }) => {
+    console.log(body.daily.data[0])
 
     if (error) {
       callback('You are not connecting with the api', undefined)
     } else if ( body.error) {S
       callback('location could not be found', undefined)
     } else {
-      callback( undefined , body.daily.data[0].summary + ` It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability} % chance of rain`
+      callback( undefined , body.daily.data[0].summary + `Todays high is ${body.daily.data[0].temperatureHigh} and todays low is ${body.daily.data[0].temperatureLow}. It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability} % chance of rain`
       )
     }
   })
